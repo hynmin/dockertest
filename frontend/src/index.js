a1300820f30e4d0eeb7ba2ab19439b2384f0e88a
miniv <hmlee108@gmail.com>
2025-11-24 18:21:00 +0900

    프론트 수정

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:8000';
 
 app.get('/', (req, res) => {
   res.send(`
@@ -9,16 +10,26 @@ app.get('/', (req, res) => {
     <body>
       <h1>도커 테스트</h1>      
         <button onclick="callAPI('/')"> Hello </button>
-        <button onclick="callAPI('/users')">사용자 목록 보기</button>
-      
+        <button onclick="callAPI('/users')">user</button>
         <div id="result"></div>
       
         <script>
+            const BACKEND_URL = '${BACKEND_URL}';
             async function callAPI(path) {
-            const response = await fetch('http://localhost:8000' + path);
-            const data = await response.json();
-            document.getElementById('result').innerHTML = 
-                '<pre>' + JSON.stringify(data, null, 2) + '</pre>';
+                const response = await fetch(BACKEND_URL + path);
+                const data = await response.json();
+                let output = '';
+                if (data.message) {
+                output = '<h2>' + data.message + '</h2>';
+                } else if (data.users) {
+                    output = '<h2>사용자 목록</h2><table border="1"><tr><th>ID</th><th>이름</th><th>이메일</th></tr>';
+                    data.users.forEach(user => {
+                    output += '<tr><td>' + user.id + '</td><td>' + user.name + '</td><td>' + user.email + '</td></tr>';
+                    });
+                    output += '</table>';
+                }
+                document.getElementById('result').innerHTML = output;
+
             }
         </script>
     </body>
@@ -26,7 +37,6 @@ app.get('/', (req, res) => {
   `);
 });
 
-// 서버 시작
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Frontend running on http://localhost:${PORT}`);
 });
